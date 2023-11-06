32f3831a1265d8af5ccfc1e04a16822a680fa67c
veekshan-p <veekshan.poshala@alcumus.com>
2023-11-06 16:17:41 +0000

    running with testng

--- src/test/java/AlcumusQa/BrowserTest.java
+++ src/test/java/AlcumusQa/BrowserTest.java
@@ -1,8 +1,8 @@
 package AlcumusQa;
 
-import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class BrowserTest {
 
